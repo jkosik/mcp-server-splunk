4f9541cedb3d7a6860797e27eaf8ebf7bcca11e4
jkosik <x@xyz.com>
2025-05-01 15:18:06 +0200

    fixes and readme

---
+++ internal/splunk/utils.go
@@ -0,0 +1,11 @@
+package splunk
+
+// getString safely gets a string from a map
+func getString(m map[string]interface{}, key string) string {
+	if v, ok := m[key]; ok {
+		if s, ok := v.(string); ok {
+			return s
+		}
+	}
+	return ""
+}
